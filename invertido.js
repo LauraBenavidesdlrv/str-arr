c304731d288261bbe342f68db687cf55f6b04d4c
LauraBenavidesdlrv <ldbenavidesr@unal.edu.co>
2021-12-27 20:05:16 -0500

    Subiendo archivo

---
+++ invertido.js
@@ -0,0 +1,9 @@
+//Sol 1
+function invertir(str) {
+    var strInv = ""
+    for ( let i = str.length - 1; i >= 0; i--) {
+      strInv += str[i];
+    }
+    console.log (strInv);
+}
+invertir("Holis");
